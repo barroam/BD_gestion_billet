40cfe23731bab033720ea96509ad7e44fe292a42
Amadou Barro <barroama23@gmail.com>
2024-04-09 04:13:33 +0000

    apport d'amélioration sur les redirections des pages

--- afficheBillet.php
+++ afficheBillet.php
@@ -31,27 +31,28 @@ foreach ($resultat as $row) {
 /* heure_reservation, mode_transport, date_depart, date__arrive, ville_depart, ville_arrive, 
 prix, status, nom_complet, numero*/ 
 ?>
+<div class="billet"> 
 
-<div class="billet">
+<div class="billetInfo"> N° <h4><?php echo $row['id']; ?> 
+ </h4> Nom Complet <h4><?php echo $row['nom_complet']; ?> 
+</h4> Numéro Téléphone: <h4><?php echo $row['numero']; ?> </h4></div> 
 
-<div class="billetInfo"> 
-<h4> </h4> <?php echo $row['id']; ?>   </h4>
-<h4><?php echo $row['nom_complet']; ?> </h4>
-<h4><?php echo $row['heure_reservation']; ?></h4>
-<h4><?php echo $row['mode_transport']; ?> </h4>
-<h4><?php echo $row['date_depart']; ?> </h4>
-<h4><?php echo $row['date__arrive']; ?> </h4>
-<h4> <?php echo $row['ville_depart']; ?> </h4>
-<h4> <?php echo $row['ville_arrive']; ?></h4>
-<h4><?php echo $row['prix']; ?></td>
-<h4>  <?php echo $row['status']; ?> </td></h4>
-<h4><?php echo $row['numero']; ?> <?php echo "<br>"; ?> </h4>
+<div class="billetDepart"><h4> <?php echo $row['ville_depart']; ?>
+ </h4> <br>  <h4><?php echo $row['date_depart']; ?> </h4> <br></div>
+<div class="billetDestination"> <h4> <?php echo $row['ville_arrive']; ?> </h4> 
+ <h4><?php echo $row['date__arrive']; ?> </h4>  </div>
+<div class="billetqualite"> <h4><?php echo $row['mode_transport']; ?> </h4> 
+ <h4>  <?php echo $row['status']; ?> </h4>  <?php echo $row['prix']; ?> </div>
+
+ <div>  <h4 ><?php echo $row['heure_reservation']; ?></h4> 
  <a href="update.php?id=<?php echo $row['id']; ?>" >Edit</a>
  <a href="supprime.php?id=<?php echo $row['id']; ?>" >Delete</a>
+</div>
 
+ </div>  
  
  <?php } ?>
-      
+
 </section>
 
 </body>
@@ -76,11 +77,23 @@ prix, status, nom_complet, numero*/
         text-decoration: none;
         color:wheat;
     }
- .billet{
-    display: flex;
-    flex-wrap: wrap;
- }
+    section{
+        display: flex;
+        flex-wrap: wrap;
 
+    }
+ .billet{
+  
+    background-color: wheat;
+    margin: 1%;
+    padding: 1%;
+   width: 28%;
+ }
+.billetInfo{
+    display: flex;
+    justify-content: space-around;
+
+}
  
    
 </style>
